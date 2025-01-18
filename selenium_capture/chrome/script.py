8c87790f71166074dfa0ded56bbf1258fa8f3d08
ayushkhaire <codeeayush@gmail.com>
2025-01-18 12:47:47 +0530

    tested browser working in script

--- selenium_capture/chrome/script.py
+++ selenium_capture/chrome/script.py
@@ -19,16 +19,17 @@ driver = webdriver.Chrome(
 default_url = "https://google.com"
 
 driver.get(default_url)
-
-from multiprocessing import shared_memory
-
-shm = shared_memory.SharedMemory(name="shared_mem")
-
-data = bytes(shm.buf[:]).rstrip(b'\x00')  
-print(f"Data read from shared memory: {data.decode('utf-8')}")
-
-shm.close()
+driver.save_screenshot("screenshot.png")
 
 print(default_url)
-time.sleep(15)
-driver.quit()
+# driver.scree
+# time.sleep(15)
+# driver.quit()
+# from multiprocessing import shared_memory
+
+# shm = shared_memory.SharedMemory(name="shared_mem")
+
+# data = bytes(shm.buf[:]).rstrip(b'\x00')  
+# print(f"Data read from shared memory: {data.decode('utf-8')}")
+
+# shm.close()
