eb33d6c4a25c01a899b9f436712b9d2a5bf35b99
ayushkhaire <codeeayush@gmail.com>
2025-01-20 16:23:15 +0530

    added memoryy manager methods

--- selenium_capture/memory_manager.py
+++ selenium_capture/memory_manager.py
@@ -9,16 +9,22 @@ class memoryManager:
         shm = shared_memory.SharedMemory(
             create=True, 
             size=128000, 
-            name=f"sms_{user_id}"
+            name=f"shared_memory_{user_id}"
         )
-        self.shared_memory_pool[f'sms_{user_id}'] = shm
+        self.shared_memory_pool[f'shared_memory_{user_id}'] = shm
         logger.debug(f"[ MEMORY ] Shared memory for user_id {user_id} is set up.")
     
-    def test_memory(self,user_id):
-        data = b"Hello from host !!"
-        self.shared_memory_pool[
-            f'sms_{user_id}'
-            ].buf[
-                :len(data)] = data
-        logger.debug(f"[ MEMORY ] host Data written to shared memory: {data.decode('utf-8')}")
-        
+    def read_memory(self, user_id):
+        try:
+            shm = shared_memory.SharedMemory(name=f'shared_memory_{user_id}', create=False)
+            buffer = shm.buf.tobytes()  # Convert buffer to bytes
+            null_index = buffer.find(b'\x00')  # Null terminator
+            if null_index != -1:
+                buffer = buffer[:null_index]  # Truncate at null terminator
+            data = buffer.decode('utf-8')
+            shm.close()
+            logger.debug(f"[ MEMORY ] Data read from shared memory for user_id {user_id}: {data}")
+            return data
+        except FileNotFoundError:
+            logger.error(f"[ MEMORY ] Shared memory block for user_id {user_id} not found.")
+            return None
