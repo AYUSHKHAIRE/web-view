1376a705baa3717af627181cf8fc119240099569
ayushkhaire <codeeayush@gmail.com>
2025-01-23 06:22:17 +0530

    starting code for browser stream . opened sockets

--- accessweb/static/js/browse.js
+++ accessweb/static/js/browse.js
@@ -2,6 +2,64 @@ function getUserId() {
   return document.getElementById("user_id").textContent;
 }
 
+function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Check if the cookie starts with the name
+      if (cookie.startsWith(name + "=")) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+async function fetchCookie() {
+  const csrftoken = getCookie("csrftoken");
+  try {
+    const response = await fetch("/browse/getcookkie/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrftoken, 
+      },
+      body: JSON.stringify({
+        cookie: document.cookie, 
+      }),
+    });
+    console.log(response.status)
+    const result = await response.json();
+    console.log(result)
+    if (result.status === "OK") {
+      console.log("Cookie saved successfully.");
+    } else {
+      console.error("Failed to save the cookie."), result.status.e;
+    }
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+}
+
+function estabilish_socket(user_id) {
+  button.textContent = "connecting socket .";
+  const sessionSocket = new WebSocket(
+    "ws://" +
+    window.location.host +
+    "/ws/" +
+    "browse/" +
+    user_id +
+    "/"
+  );
+  setTimeout(() => {
+    sessionSocket.send(`connected to session ${user_id}`);
+  }, 1000);
+  button.textContent = "connected socket ."
+}
+
 function startAsession(userid) {
   console.log("Starting the session for user:", userid);
   const url = `${window.location.origin}/browse/start_session/${userid}`;
@@ -18,6 +76,7 @@ function startAsession(userid) {
       if (data.status == 'OK') {
         console.log("Session started successfully:", data);
         button.textContent = "session started successfully . loading screens ..."
+        estabilish_socket(userid);
       } else {
         console.error(
           "Session failed to start:",
@@ -28,11 +87,14 @@ function startAsession(userid) {
     .catch((error) => {
       console.error("An error occurred:", error.message);
     });
-}
-
-// Attach event listener
-const button = document.querySelector("#startSessionButton");
-button.addEventListener("click", function () {
-  const userid = getUserId(); // Fetch the user ID when the button is clicked
+  }
+  
+  
+  // Attach event listener
+  const button = document.querySelector("#startSessionButton");
+  button.addEventListener("click", function () {
+    const userid = getUserId(); // Fetch the user ID when the button is clicked
   startAsession(userid);
 });
+
+fetchCookie();
