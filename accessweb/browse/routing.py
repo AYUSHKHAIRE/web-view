a4e8acb76b9a7a1f7237298d3e60665d29726925
ayushkhaire <codeeayush@gmail.com>
2025-01-23 06:18:04 +0530

    writing router for it

---
+++ accessweb/browse/routing.py
@@ -0,0 +1,8 @@
+# chat/routing.py
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r"ws/browse/(?P<user_id>[a-f0-9\-]+)/$", consumers.WebSocketConsumer.as_asgi()),
+]
