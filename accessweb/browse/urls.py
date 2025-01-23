55bee553518d0ed49e24692bbcb4c7f30491d8a5
ayushkhaire <codeeayush@gmail.com>
2025-01-23 06:19:15 +0530

    added url to get cookie

--- accessweb/browse/urls.py
+++ accessweb/browse/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='browse'),
     path('start_session/<str:user_id>/', views.start_session, name=''),
+    path('getcookkie/', views.getCookie, name=''),
 ]
