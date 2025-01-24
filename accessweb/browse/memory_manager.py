cfd10f5eac3dd34dc74fb13ecf09c3eeedffd60e
ayushkhaire <codeeayush@gmail.com>
2025-01-24 12:31:46 +0530

    added code for bytes streaming

--- accessweb/browse/memory_manager.py
+++ accessweb/browse/memory_manager.py
@@ -8,7 +8,7 @@ class memoryManager:
         self.clean_memory(user_id)
         shm = shared_memory.SharedMemory(
             create=True, 
-            size=1280000, 
+            size=6221440,  
             name=f"shared_memory_{user_id}"
         )
         self.shared_memory_pool[f'shared_memory_{user_id}'] = shm
@@ -17,16 +17,24 @@ class memoryManager:
     def read_memory(self, user_id):
         try:
             shm = shared_memory.SharedMemory(name=f'shared_memory_{user_id}', create=False)
-            buffer = shm.buf.tobytes()  # Convert buffer to bytes
-            null_index = buffer.find(b'\x00')  # Null terminator
+            buffer = shm.buf.tobytes()  
+            null_index = len(buffer) - 1
+            while null_index >= 0 and buffer[null_index] == 0:
+                null_index -= 1
+
+            # Truncate at the null terminator
+            buffer = buffer[:null_index + 1]  # +1 because index is zero-based
             if null_index != -1:
-                buffer = buffer[:null_index]  # Truncate at null terminator
-            data = buffer.decode('utf-8')
+                buffer = buffer[:null_index]  # Truncate at the null terminator
             shm.close()
-            logger.debug(f"[ MEMORY ] Data read from shared memory for user_id {user_id}: {data}")
-            return data
+            logger.debug(f"[ MEMORY ] Binary data read from shared memory for user_id {user_id}.{len(buffer)}")
+            return buffer
+
         except FileNotFoundError:
             logger.error(f"[ MEMORY ] Shared memory block for user_id {user_id} not found.")
+            return None
+        except Exception as e:
+            logger.error(f"[ MEMORY ] Failed to read from shared memory for user_id {user_id}: {e}")
             return None
 
     def clean_memory(self,user_id):
