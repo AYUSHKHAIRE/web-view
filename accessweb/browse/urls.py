b5c1450c5ec7edb862d25c737a956df44c131675
ayushkhaire <codeeayush@gmail.com>
2025-01-28 20:43:59 +0530

    added screen size support for url

--- accessweb/browse/urls.py
+++ accessweb/browse/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.index, name='browse'),
-    path('start_session/<str:user_id>/', views.start_session, name=''),
+    path('start_session/<str:user_id>/<str:screen_dex>/', views.start_session, name=''),
     path('getcookkie/', views.getCookie, name=''),
 ]
