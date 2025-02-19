95421ec752f96de5d75e6c3677efe7af1127e2bb
ayushkhaire <codeeayush@gmail.com>
2025-02-19 13:17:17 +0530

    restructured the code

--- accessweb/browse/routing.py
+++ accessweb/browse/routing.py
@@ -4,5 +4,8 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/browse/(?P<user_id>[a-f0-9\-]+)/$", consumers.WebSocketConsumer.as_asgi()),
+    re_path(
+        r"ws/browse/(?P<user_id>[a-f0-9\-]+)/$", 
+        consumers.WebSocketConsumer.as_asgi()
+    ),
 ]
