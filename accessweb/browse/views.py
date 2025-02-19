6cf5470ce80f374e1c7735a25db45e38d4bd36c7
ayushkhaire <codeeayush@gmail.com>
2025-02-19 13:17:37 +0530

    restructured the code

--- accessweb/browse/views.py
+++ accessweb/browse/views.py
@@ -19,43 +19,104 @@ SSM = sessionManager()
 # WSS.start_in_thread()
 
 @login_required
-def index(request):
-    user_id = UserProfile.objects.get(user = request.user.id).uuid
-    return render(request,"browse/main.html", {'user_id': user_id})
+def index(
+    request
+):
+    user_id = UserProfile.objects.get(
+        user = request.user.id
+    ).uuid
+    return render(
+        request,
+        "browse/main.html", 
+        {
+            'user_id': user_id
+        }
+    )
 
 @login_required
-def getCookie(request):
-    user_profile = UserProfile.objects.get(user=request.user.id)
+def getCookie(
+    request
+):
+    user_profile = UserProfile.objects.get(
+        user=request.user.id
+    )
     user_id = str(user_profile.uuid)  
     if request.method == "POST":
         try:
-            data = json.loads(request.body)
+            data = json.loads(
+                request.body
+            )
             cookie = data.get('cookie')
             if not cookie:
-                return JsonResponse({'status': "Error", 'message': "Cookie is required."}, safe=False)
+                return JsonResponse(
+                    {
+                        'status': "Error", 
+                        'message': "Cookie is required."
+                    },
+                    safe=False
+                )
             file_data = {
                 'user_id': user_id, 
                 'cookie': cookie
             }
             directory_path = f'{BASE_DIR}/browse/docker_containers/docker_{user_id}/'
-            os.makedirs(directory_path, exist_ok=True)
-            with open(f'{directory_path}/cookie.json', "w") as f:
-                json.dump(file_data, f)
-            return JsonResponse({'status': "OK"}, safe=False)
+            os.makedirs(
+                directory_path, 
+                exist_ok=True
+            )
+            with open(
+                f'{directory_path}/cookie.json', 
+                "w"
+            ) as f:
+                json.dump(
+                    file_data, 
+                    f
+                )
+            return JsonResponse(
+                {
+                    'status': "OK"
+                }, 
+                safe=False
+            )
         except Exception as e:
-            return JsonResponse({'status': "Error", 'message': str(e)}, safe=False)
-    return JsonResponse({'status': "Error", 'message': "Invalid request method."}, safe=False)
+            return JsonResponse(
+                {
+                    'status': "Error", 
+                    'message': str(e)
+                }, 
+                safe=False
+            )
+    return JsonResponse(
+        {
+            'status': "Error", 
+            'message': "Invalid request method."
+        }, 
+        safe=False
+    )
 
 @login_required
-def start_session(request,user_id,screen_dex):
+def start_session(
+    request,
+    user_id,
+    screen_dex
+):
     time.sleep(0.1)
     logger.debug(f"received and passed {user_id} to main server and container .")
-    SSM.setup_docker(user_id=user_id,screendex=screen_dex)
+    SSM.setup_docker(
+        user_id=user_id,
+        screendex=screen_dex
+    )
     time.sleep(0.1)
-    MM.setup_memory(user_id=user_id)
+    MM.setup_memory(
+        user_id=user_id
+    )
     time.sleep(0.1)
-    data = MM.read_memory(user_id=user_id)
+    data = MM.read_memory(
+        user_id=user_id
+    )
     jsn_to_send = {
         'status':'OK'
     }
-    return JsonResponse(jsn_to_send)
+    return JsonResponse(
+        jsn_to_send
+    )
