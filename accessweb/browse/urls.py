2b1dec27f6768e90cae314a765cf0b226731864f
ayushkhaire <codeeayush@gmail.com>
2025-02-19 13:17:32 +0530

    restructured the code

--- accessweb/browse/urls.py
+++ accessweb/browse/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
-    path('', views.index, name='browse'),
-    path('start_session/<str:user_id>/<str:screen_dex>/', views.start_session, name=''),
-    path('getcookkie/', views.getCookie, name=''),
+    path(
+        '',
+        views.index,
+        name='browse'
+    ),
+    path(
+        'start_session/<str:user_id>/<str:screen_dex>/',
+        views.start_session, 
+        name=''
+    ),
+    path(
+        'getcookkie/', 
+        views.getCookie, 
+        name=''
+    ),
 ]
