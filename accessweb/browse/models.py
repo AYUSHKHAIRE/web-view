709a951b8eeaa6adb7e9d9286a9142a1d72fd62c
ayushkhaire <codeeayush@gmail.com>
2025-02-19 13:17:05 +0530

    restructured the code

--- accessweb/browse/models.py
+++ accessweb/browse/models.py
@@ -2,9 +2,19 @@ import uuid
 from django.contrib.auth.models import User
 from django.db import models
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+class UserProfile(
+    models.Model
+):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='profile'
+    )
+    uuid = models.UUIDField(
+        default=uuid.uuid4, 
+        editable=False, 
+        unique=True
+    )
     
     def __str__(self):
             return f"Profile of {self.user.username}"
