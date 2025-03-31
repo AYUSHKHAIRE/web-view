d809d5f57cb498cb081f73561960e3624f7de9c8
ayushkhaire <codeeayush@gmail.com>
2025-03-31 23:10:35 +0530

    deployment P1

--- setup.sh
+++ setup.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
 
 # Update package list
-apt update 
+sudo apt update 
 
 # Install required dependencies
-apt install -y ca-certificates curl gnupg lsb-release
+sudo apt install -y ca-certificates curl gnupg lsb-release
 
 # Setup Docker repository
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | tee /etc/apt/keyrings/docker.asc > /dev/null
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo tee /etc/apt/keyrings/docker.asc > /dev/null
 chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Update and install Docker
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# install docker
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # Start and enable Docker service
-systemctl start docker
-systemctl enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
 
 # Install Redis
-apt install -y redis-server
+sudo apt install -y redis-server
 
 # Start and enable Redis service
-systemctl start redis-server
-systemctl enable redis-server
+sudo systemctl start redis-server
+sudo systemctl enable redis-server
+sudo systemctl status redis-server
 
 # Print versions to verify installation
 docker --version
 redis-server --version
+
+# install python3
+sudo apt install -y python3 python3-pip python3-venv
